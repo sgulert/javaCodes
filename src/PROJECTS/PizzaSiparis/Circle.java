115202db0027679fbe947f3ce4fc36ce8df627ce
sumeyye guler tuzuner <gulertuzuner@gmail.com>
2023-03-18 23:14:54 +0300

    pizza order other classes

---
+++ src/PROJECTS/PizzaSiparis/Circle.java
@@ -0,0 +1,20 @@
+package PROJECTS.PizzaSiparis;
+
+public class Circle extends IShape{
+private double _radius;
+final double PI=3.14; 
+public Circle(double _radius) {
+	super();
+	this._radius=_radius;
+}
+	@Override
+	public double getArea() {
+		return PI*_radius*_radius;
+
+	}
+	@Override
+	public String toString() {
+		return "Circle radius=" + _radius + ", Area" + getArea() ;
+	}
+
+}
---
+++ src/PROJECTS/PizzaSiparis/PizzaDeal.java
@@ -0,0 +1,15 @@
+package PROJECTS.PizzaSiparis;
+
+public class PizzaDeal {
+               //Pizza class demek p obje ismi pizza ayse 
+	public double deal(Pizza p) {
+
+		return p.getPrice();
+	}
+	public boolean betterDeal(Pizza p1, Pizza p2) {
+
+		return true;
+	}
+
+}
+
